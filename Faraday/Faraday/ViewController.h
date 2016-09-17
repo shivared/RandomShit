30fbcc8236e25699337d4d0490785b244a295c50
Pavan Shivareddy <Pavan@Pavans-MacBook-Pro.local>
2016-09-16 21:12:46 -0700

    Init Faraday

---
+++ Faraday/Faraday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Faraday
+//
+//  Created by Pavan on 9/16/16.
+//  Copyright © 2016 Pavan Shivareddy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+

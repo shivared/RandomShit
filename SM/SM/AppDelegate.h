918a967216df27bb91219629d8741ef3dbf8f919
Pavan Shivareddy <Pavan@Pavans-MacBook-Pro.local>
2016-09-16 22:54:04 -0700

    SM added

---
+++ SM/SM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SM
+//
+//  Created by Pavan on 9/16/16.
+//  Copyright © 2016 Pavan Shivareddy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SM/SM/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SM
+//
+//  Created by Pavan on 9/16/16.
+//  Copyright © 2016 Pavan Shivareddy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}

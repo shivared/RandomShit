4869da8c63011251391cb7efbae519d66da8e18f
Pavan Shivareddy <Pavan@Pavans-MacBook-Pro.local>
2016-09-16 21:07:05 -0700

    Initial Commit

---
+++ VR/VR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VR
+//
+//  Created by Pavan on 9/16/16.
+//  Copyright © 2016 Pavan Shivareddy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ VR/VR/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  VR
+//
+//  Created by Pavan on 9/16/16.
+//  Copyright © 2016 Pavan Shivareddy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
